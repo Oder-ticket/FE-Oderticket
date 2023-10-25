bb65531962203fca1a80dd83ef936e8b2a0814a6
NNQA <anhla12h@gmail.com>
2023-10-25 15:05:17 +0700

    feat: add layout and router for project

---
+++ orderticket/src/lib/layout/layout.authentication.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LayoutAuthentication = ({ children }) => {
+  return (
+    <div className="h-screen">{children}</div>
+  )
+}
+
+export default LayoutAuthentication
