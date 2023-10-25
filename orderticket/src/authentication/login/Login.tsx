ba48a4ba48549d8d8c3af2b69077432d68cb74a6
NNQA <anhla12h@gmail.com>
2023-10-25 15:06:21 +0700

    feat: add layout and router for project

---
+++ orderticket/src/authentication/login/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LayoutAuthentication from "../../lib/layout/layout.authentication";
+
+function Login() {
+  return (
+    <LayoutAuthentication>
+      <div>asdsa</div>
+    </LayoutAuthentication>
+  );
+}
+
+export default Login;
