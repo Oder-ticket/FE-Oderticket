c39d39e9444851b171dd6bedc7617e2fb60bb7b6
Huynh Nhat Vuong <huynhnhatvuong01012002@gmail.com>
2023-10-25 17:58:37 +0700

    changes

--- orderticket/src/App.jsx
+++ orderticket/src/App.jsx
@@ -3,13 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./authentication/login/Login";
 
 function App() {
-
   return (
     // <Routes>
     //     <Route path={`/login`} element={<Login></Login>}></Route>
     //   </Routes>
-      <Login></Login>
-
+    <Login></Login>
   );
 }
 
--- orderticket/src/lib/components/FormComponents.jsx
+++ orderticket/src/lib/components/FormComponents.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import {Form, Input} from "antd";
+import { Form, Input } from "antd";
 //amount
 function FormComponents(data) {
   return (
-      <div className={`te`}>
-          <h1 key={"asd"}>{data.data.title}</h1>
-          <Form>
-              {console.log(data.data.array)}
-              {
-                  data?.data?.array?.map((item,index)=>{
-                      return (
-                          <Form.Item key = {index}>
-                              <Input/>
-                          </Form.Item>
-                      )
-                  })
-              }
-
-          </Form>
-      </div>
-  )
+    <div
+      className={`p-16 ${
+        data.data.title === "login" ? `w-[500px]` : "w-[400px]"
+      }`}
+    >
+      <h1 key={"asd"}>{data.data.title}</h1>
+      <Form>
+        {console.log(data.data.array)}
+        {data?.data?.array?.map((item, index) => {
+          return (
+            <Form.Item key={index}>
+              <Input className="w-full" />
+            </Form.Item>
+          );
+        })}
+      </Form>
+    </div>
+  );
 }
-
 
 export default FormComponents;
